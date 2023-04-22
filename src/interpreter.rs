2a4a70b23161655b79d18cd064c48dbf68e59a66
Yamashita <jey.togo@gmail.com>
2023-04-22 21:01:38 +0900

    done

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,7 +1,6 @@
-use std::{fmt::{Error, format}, ops::Index, io::{self, stdin, BufRead}};
-
-// use crate::tokenizer;
-
+use std::{
+    io::{self},
+};
 
 #[derive(Debug, Clone, Copy)]
 pub enum Token {
@@ -13,8 +12,10 @@ pub enum Token {
     EOL, // ] End of a Loop
     DOT, // . DOT
     CMM, // , Comma
-    None
-}pub struct MemManager {
+    None,
+}
+
+pub struct Interpreter {
     mem: Vec<u8>,
     head: usize,
     tokens: Vec<Token>,
@@ -23,49 +24,28 @@ pub enum Token {
 }
 
 pub fn tokenize(input: Vec<u8>) -> Vec<Token> {
-    let x: Vec<char> = input.iter().map(|x| {
-        match ascii_to_char(*x) {
-            Ok(x) => x,
-            Err(e) => '_',
-        }
-    }).collect();
-
-    x.iter().map(|x| {
-        match *x {
-            '+' => Token::INC,
-            '-' => Token::DEC,
-            '>' => Token::GTS,
-            '<' => Token::LTS,
-            '[' => Token::BOL,
-            ']' => Token::EOL,
-            '.' => Token::DOT,
-            ',' => Token::CMM,
+    input
+        .iter()
+        .map(|&c| match c {
+            b'+' => Token::INC,
+            b'-' => Token::DEC,
+            b'>' => Token::GTS,
+            b'<' => Token::LTS,
+            b'[' => Token::BOL,
+            b']' => Token::EOL,
+            b'.' => Token::DOT,
+            b',' => Token::CMM,
             _ => Token::None,
-        }
-    }).collect()
+        })
+        .collect()
 }
 
-pub fn tokenize_v2(input: Vec<u8>) -> Vec<Token> {
-    input.iter().map(|&c| match c {
-        b'+' => Token::INC,
-        b'-' => Token::DEC,
-        b'>' => Token::GTS,
-        b'<' => Token::LTS,
-        b'[' => Token::BOL,
-        b']' => Token::EOL,
-        b'.' => Token::DOT,
-        b',' => Token::CMM,
-        _ => Token::None,
-    }).collect()
-}
+impl Interpreter {
+    pub fn init(input: Vec<u8>) -> Interpreter {
+        let tokens = tokenize(input);
+        // println!("{:?}", tokens);
 
-impl MemManager {
-    pub fn init(input: Vec<u8>) -> MemManager {
-        // print!("init");
-        let tokens = tokenize_v2(input);
-        println!("{:?}", tokens);
-
-        MemManager {
+        Interpreter {
             mem: vec![0; 1000],
             head: 0,
             tokens: tokens,
@@ -74,13 +54,12 @@ impl MemManager {
         }
     }
 
-    fn inc (&mut self) {
-        // self.mem[self.head] += 1;
+    fn inc(&mut self) {
         self.mem[self.head] = (self.mem[self.head] + 1) % 255;
         Self::print_current_status(&self, '+');
     }
 
-    fn dec (&mut self) {
+    fn dec(&mut self) {
         if self.mem[self.head] <= 0 {
             panic!("dec error");
         }
@@ -89,46 +68,36 @@ impl MemManager {
         Self::print_current_status(&self, '-');
     }
 
-    fn move_right (&mut self) {
-        // println!("current: {} {}", self.head, self.mem[self.head]);
-
-        // print!(">");
+    fn move_right(&mut self) {
         self.head += 1;
         Self::print_current_status(&self, '>');
     }
 
-    fn move_left (&mut self) {
-        // println!("current: {} {}", self.head, self.mem[self.head]);
+    fn move_left(&mut self) {
         if self.head == 0 {
             return;
         }
-        // print!("<");
+
         self.head -= 1;
         Self::print_current_status(&self, '<');
     }
 
-    fn stdin (&mut self) {
-        // print!("input: ");
+    fn stdin(&mut self) {
         if self.stdin_buf.len() != 0 {
             self.mem[self.head] = self.stdin_buf.remove(0);
             return;
         }
 
         let mut c = String::new();
-
-        io::stdin()
-            .read_line(&mut c)
-            .expect("Failed to read line");
+        io::stdin().read_line(&mut c).expect("Failed to read line");
 
         self.stdin_buf = c.into_bytes();
-
         self.mem[self.head] = self.stdin_buf.remove(0);
 
         Self::print_current_status(&self, ',');
     }
 
     fn show_head_char(&self) {
-        // Self::print_current_status(&self, '.');
         print!("{}", self.mem[self.head] as char);
         Self::print_current_status(&self, '.');
     }
@@ -137,9 +106,10 @@ impl MemManager {
         self.mem[self.head] > 0
     }
 
+    /* デバッグ用メソッド */
     fn print_current_status(&self, _token: char) {
         let token = self.tokens[self.tokens_cursor];
-        let c = match token {
+        let _c = match token {
             Token::INC => '+',
             Token::DEC => '-',
             Token::GTS => '>',
@@ -148,15 +118,15 @@ impl MemManager {
             Token::CMM => ',',
             Token::BOL => '[',
             Token::EOL => ']',
-            Token::None => '_'
+            Token::None => '_',
         };
 
+        // デバッグ時は以下の行をコメントアウトする。
         // println!("{} | {:?} head: {} cell: {} ", self.tokens_cursor, c, self.head, self.mem[self.head]);
     }
 
-    pub fn run(&mut self) -> Result<String, String> {
+    pub fn run(&mut self) {
         while self.tokens_cursor < self.tokens.len() {
-            // let target = Self::ascii_to_char(self.tokens[self.tokens_cursor])?;
 
             match self.tokens[self.tokens_cursor] {
                 Token::INC => self.inc(),
@@ -166,7 +136,6 @@ impl MemManager {
                 Token::DOT => self.show_head_char(),
                 Token::CMM => self.stdin(),
                 Token::BOL => {
-                    // println!("-----");
                     if !self.is_current_cell_alive() {
                         let mut count = 1;
 
@@ -200,291 +169,10 @@ impl MemManager {
 
                     self.print_current_status(']');
                 },
-                _ => {},
+                _ => {}
             };
 
             self.tokens_cursor += 1;
-        };
-
-        Ok(format!("Ok"))
-    }
-
-    fn ascii_to_char(x: u8) -> Result<char, String> {
-        if !x.is_ascii() {
-            return Err(format!("ERR01: {x} is not ascii code").to_string());
         }
-
-        Ok(x as char)
     }
-
-    fn jump_forward(tokens: &[Token], cursor: usize) -> Result<usize, String> {
-        let mut level = 0;
-
-        for i in 0..tokens.len() {
-            // let target = Self::ascii_to_char(tokens[i])?;
-
-            match tokens[i] {
-                Token::EOL => {
-                    if level == 0 {
-                        println!("jump!!!!!");
-                        return Ok(i+cursor+1);
-                    }
-                    level -= 1;
-                },
-                Token::BOL => {
-                    level += 1;
-                },
-                _ => {}
-            }
-        }
-
-        panic!("Unmatched ']'");
-    }
-
-    fn jump_backward(tokens: &[Token]) -> Result<usize, String> {
-        let mut level = 0;
-
-        for i in (0..tokens.len()).rev() {
-            // let target = Self::ascii_to_char(tokens[i])?;
-            // println!("{:?}", tokens[i]);
-
-            match tokens[i] {
-
-            Token::EOL => {
-                // println!("{:?} ----", tokens[i]);
-                level += 1;
-            },
-            Token::BOL => {
-                // println!("{:?} ----", tokens[i]);
-
-                if level == 0 {
-                    return Ok(i);
-                }
-                level -= 1;
-                },
-                _ => {}
-            }
-        }
-
-        panic!("Unmatched '['");
-    }
-
-    fn char_to_ascii(x: char) -> Result<u8, String> {
-        if !x.is_ascii() {
-            return Err(format!("ERR02: {x} is not ascii code").to_string());
-        }
-
-        Ok(x as u8)
-    }
-}
-
-
-
-pub struct Interpreter;
-
-impl Interpreter {
-    // pub fn consume(tokens: &mut Vec<u8>, memory_manager: &mut MemManager) {
-    //     tokens.iter().enumerate().for_each(|(index, x)| {
-    //         if !x.is_ascii() {
-    //             return;
-    //         }
-
-    //         let target_char = *x as char;
-    //         match target_char {
-    //             '+' => memory_manager.inc(),
-    //             '-' => memory_manager.dec(),
-    //             '>' => memory_manager.move_right(),
-    //             '<' => memory_manager.move_left(),
-    //             '.' => memory_manager.show_head_char(),
-    //             '[' => {
-    //                 let end_loop_index = match Self::search_pair_elements(&tokens[index..tokens.len()]) {
-    //                     Ok(end) => end,
-    //                     Err(e) => return (),
-    //                 } + index;
-
-    //                 loop {
-    //                     // if !memory_manager.is_current_cell_alive()
-    //                     // Interpreter::consume(&mut tokens[index..end_loop_index].to_vec(), memory_manager);
-    //                 }
-    //             },
-    //             // ']' => ,
-    //         }
-    //     });
-    // }
-
-    pub fn consume_v2(tokens: &mut Vec<u8>, memory_manager: &mut MemManager) -> Result<String, String> {
-        let mut cursor: usize = 0;
-        let mut current_start_loop_index = 0;
-        let mut loop_stack: Vec<usize> = Vec::new();
-
-        while cursor < tokens.len() {
-            let target = Self::ascii_to_char(tokens[cursor])?;
-
-            print!("{cursor} | {target} ");
-            // println!("{target}");
-
-            match target {
-                '+' => memory_manager.inc(),
-                '-' => memory_manager.dec(),
-                '>' => memory_manager.move_right(),
-                '<' => memory_manager.move_left(),
-                '.' => memory_manager.show_head_char(),
-                ',' => memory_manager.stdin(),
-                '[' => {
-
-                    // let (_start_loop_index, _end_loop_index) = Self::search_pair_elements_v2(cursor, &tokens[cursor+1..tokens.len()])?;
-
-                    // if !loop_stack.contains(&start_loop_index) {
-                    //     // loop_stack.push(start_loop_index);
-                    //     cursor = Self::jump_backward(&tokens[cursor..], cursor);
-                    // }
-                    // println!("push {:?}", loop_stack);
-
-                    if !memory_manager.is_current_cell_alive() {
-                        cursor = Self::jump_forward(&tokens[cursor..], cursor)?;
-                    }
-                    // else {
-                    //     current_start_loop_index = start_loop_index;
-                    // }
-
-                    memory_manager.print_current_status('[');
-                },
-                ']' => {
-                    if memory_manager.is_current_cell_alive() {
-                        // スタック末尾のインデックスを参照し、ループの先頭に戻る
-                        cursor = Self::jump_backward(&tokens[..cursor])?;
-                        // if let Some(target_start_index) = loop_stack.get(loop_stack.len() - 1) {
-                        //     cursor = *target_start_index;
-                        // }
-                    } else {
-                        // ループを抜ける。対象のidxは削除
-                        loop_stack.pop();
-                        // println!("pop {:?}", loop_stack);
-                    }
-
-                    memory_manager.print_current_status(']');
-                    // println!("jump back -------------");
-                },
-                _ => {}
-            }
-
-            cursor += 1;
-        }
-
-        Ok(format!("Ok"))
-    }
-
-    pub fn search_pair_elements_v2(start: usize, slice: &[u8]) -> Result<(usize, usize), String> {
-        let mut end = 0;
-        let mut cursor: usize = 0;
-
-        let mut stack: Vec<bool> = Vec::new();
-        // println!("stack: {}", stack.len());
-
-        while cursor < slice.len() {
-            // if cursor >= slice.len() {
-            //     return Ok(format!("ok"))
-            // }
-
-            let target = Self::ascii_to_char(slice[cursor])?;
-
-            // println!("target: {target}");
-
-            match target {
-                '[' => {
-                    stack.push(true);
-                }
-                ']' => {
-                    if stack.len() == 0 {
-                        // println!("stack is emp");
-                        end = cursor;
-                        break;
-                    }
-
-                    stack.pop();
-                }
-                _ => ()
-            }
-
-            cursor += 1;
-        }
-
-        if end == 0 {
-            // println!("xxxxxxxxxxxx!!");
-            return Err(format!("error"));
-        }
-
-        // println!("st: {start}, en: {}", end+start+1);
-        Ok((start, end+start+1)) // endはゼロオリジンだから+1する必要がある。
-    }
-
-    fn ascii_to_char(x: u8) -> Result<char, String> {
-        if !x.is_ascii() {
-            return Err(format!("ERR01: {x} is not ascii code").to_string());
-        }
-
-        Ok(x as char)
-    }
-
-    fn jump_forward(tokens: &[u8], cursor: usize) -> Result<usize, String> {
-        let mut level = 0;
-
-        for i in 0..tokens.len() {
-            let target = Self::ascii_to_char(tokens[i])?;
-
-            match target {
-                '[' => {
-                    level += 1;
-                },
-                ']' => {
-                    if level == 0 {
-                        return Ok(i+cursor+1);
-                    }
-                    level -= 1;
-                },
-                _ => {}
-            }
-        }
-
-        panic!("Unmatched ']'");
-    }
-
-    fn jump_backward(tokens: &[u8]) -> Result<usize, String> {
-        let mut level = 0;
-
-        for i in (0..tokens.len()).rev() {
-            let target = Self::ascii_to_char(tokens[i])?;
-
-            match target {
-                '[' => {
-                    if level == 0 {
-                        return Ok(i);
-                    }
-                    level -= 1;
-                },
-                ']' => {
-                    level += 1;
-                },
-                _ => {}
-            }
-        }
-
-        panic!("Unmatched '['");
-    }
-
-    fn char_to_ascii(x: char) -> Result<u8, String> {
-        if !x.is_ascii() {
-            return Err(format!("ERR02: {x} is not ascii code").to_string());
-        }
-
-        Ok(x as u8)
-    }
-}
-
-fn ascii_to_char(x: u8) -> Result<char, String> {
-    if !x.is_ascii() {
-        return Err(format!("ERR01: {x} is not ascii code").to_string());
-    }
-
-    Ok(x as char)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,17 @@
 use std::{env, fs::File, io::Read};
 
-// mod tokenizer;
 mod interpreter;
-mod consumer;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
     let filename: &str = &args[1];
-    // println!("filename: {filename}");
 
-    let mut file = File::open(filename).expect("file not found...");
-
-    // let mut contents_string: String = String::new();
     let mut contents_ascii_vec: Vec<u8> = Vec::new();
 
+    let mut file = File::open(filename).expect("file not found...");
     file.read_to_end(&mut contents_ascii_vec).expect("something went wrong reading the file");
 
-    // println!("With text: \n{:?}", contents_string);
-    println!("size: {:?}", contents_ascii_vec.len());
+    // println!("size: {:?}", contents_ascii_vec.len());
 
-    let LENGTH: usize = contents_ascii_vec.len();
-    let mut memory: Vec<u8> = vec![8; LENGTH].to_vec();
-
-    let mut interpreter = interpreter::MemManager::init(contents_ascii_vec);
+    let mut interpreter = interpreter::Interpreter::init(contents_ascii_vec);
     interpreter.run();
 }
